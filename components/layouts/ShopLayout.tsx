c013097beb4d104e56181b959a91ba812411f375
Sergio Colque <raidanearth@gmail.com>
2022-12-29 12:49:49 -0300

    layouts
    Estructura de directorios y layouts

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import { FC } from "react";
+import React, { PropsWithChildren } from "react";
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<PropsWithChildren<Props>> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+
+        <meta name="description" content={pageDescription} />
+
+        <meta name="ogititle" content={title} />
+        <meta name="og:description" content={pageDescription} />
+
+        {
+            imageFullUrl && (
+                <meta name="og:image" content={imageFullUrl} />
+            )
+        }
+      </Head>
+
+      <nav>{/* TODO: Navbar */}</nav>
+      {/* TODO: Sidebar */}
+
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0 30px",
+        }}
+      >
+        {children}
+      </main>
+
+      {/* Footer */}
+
+      <footer>
+        {/*TODO: mi custom footer */}
+      </footer>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import Typography from '@mui/material/Typography';
 import type { NextPage } from 'next'
+import { ShopLayout } from '../components/layouts';
 
 
 const Home: NextPage = () => {
   return (
-   <h1>Chelis-Shop</h1>
+   <ShopLayout title={'Chelis-Shop - Home'} pageDescription={'Encuentra los mejores productos de Chelis aquí'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 
