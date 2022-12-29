70db0034495c32d333cacc38ea63ebac1363e9c0
Sergio Colque <raidanearth@gmail.com>
2022-12-29 17:13:33 -0300

    Navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,20 +1,72 @@
-import NextLink from 'next/link';
+import NextLink from "next/link";
 
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import Link from 'next/link';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+import Badge from "@mui/material/Badge";
 
 export const Navbar = () => {
   return (
     <AppBar>
-        <Toolbar>
-            <NextLink href='/' passHref>
-                <Link href='/'>
-                    <Typography variant='h6' component='h1'>Chelis </Typography>
-                    <Typography sx={{ ml: 0.5}}>shop</Typography>
-                </Link>
+      <Toolbar>
+        <NextLink href="/" passHref>
+          <Link display="flex" alignItems="center">
+            <Typography variant="h6">Chelis </Typography>
+            <Typography sx={{ ml: 0.5 }}>shop</Typography>
+          </Link>
 
+          {/* todo flex */}
+
+          <Box flex={1} />
+
+          <Box sx={{ display: { xs: 'none', sm: 'block'} }}>
+            <NextLink href="/category/men" passHref>
+              <Link>
+                <Button>Hombres</Button>
+              </Link>
             </NextLink>
-        </Toolbar>
+            <NextLink href="/category/women" passHref>
+              <Link>
+                <Button>Mujeres</Button>
+              </Link>
+            </NextLink>
+            <NextLink href="/category/kid" passHref>
+              <Link>
+                <Button>Niños</Button>
+              </Link>
+            </NextLink>
+          </Box>
+
+          <Box flex={1} />
+
+          {/* todo flex */}
+
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+
+          <NextLink href="/cart" passHref>
+            <Link>
+              <IconButton>
+                <Badge badgeContent={ 2 } color='secondary'>
+                    <ShoppingCartOutlined />
+                </Badge>                
+              </IconButton>
+            </Link>
+          </NextLink>
+
+          <Button>
+            Menu
+          </Button>
+        </NextLink>
+      </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
