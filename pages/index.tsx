fcfcc987453cac8631f7df5c76a498ef8ef866a4
Sergio Colque <raidanearth@gmail.com>
2023-01-01 18:02:56 -0300

    componentes para mostrar productos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material
 
 import { ShopLayout } from '../components/layouts';
 import { initialData } from '../database/products';
+import { ProductList } from '../components/products';
 
 
 const Home: NextPage = () => {
@@ -11,23 +12,9 @@ const Home: NextPage = () => {
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <Grid container spacing={4}>
-        {
-        initialData.products.map( product => (
-          <Grid item xs={6} sm={4} key={ product.slug }>
-            <Card>
-              <CardActionArea>
-                <CardMedia 
-                component='img'
-                image={ `products/${ product.images[0] }`}
-                alt={ product.title }
-              />
-              </CardActionArea>
-            </Card>
-          </Grid>    
-          ))
-        }
-    </Grid>
+      <ProductList
+        products={ initialData.products as any }
+      />
 
     </ShopLayout>
 
